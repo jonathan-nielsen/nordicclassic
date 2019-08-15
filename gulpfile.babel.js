3119c16ce41cac99d97968e815c418850fcd3617
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2019-08-15 11:25:54 +0200

    Added gulp, bootstrap and media

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,15 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import babel from 'gulp-babel';
-import concat from 'gulp-concat';
-import gulp from 'gulp';
-import sourcemaps from 'gulp-sourcemaps';
 import cleanCSS from 'gulp-clean-css';
+import concat from 'gulp-concat';
+import fs from 'fs-extra';
+import gulp from 'gulp';
+import path from 'path';
+import sourcemaps from 'gulp-sourcemaps';
+import sass from 'gulp-sass';
+
+sass.compiler = require('node-sass');
 
 function js() {
 	return gulp.src('src/js/**/*.js')
@@ -13,15 +20,31 @@ function js() {
 		.pipe(gulp.dest('dist'));
 };
 
-function css() {
-	return gulp.src('src/css/**/*.{scss|css}')
+async function css() {
+	try {
+		await fs.copy('src/css/bootstrap/', 'dist/css/bootstrap/');
+	} catch (err) {
+		return console.error(err)
+	}
+
+	return gulp.src('src/css/**/*.scss')
 		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
 		.pipe(cleanCSS())
 		.pipe(concat('style.css'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist/css'));
 }
 
-exports.default = gulp.parallel(js, css);
+async function html() {
+	try {
+		await fs.copy('src/html/index.html', 'dist/index.html');
+	} catch (err) {
+		return console.error(err)
+	}
+}
+
+exports.default = gulp.parallel(js, css, html);
+exports.html = html;
 exports.js = js;
 exports.css = css;
